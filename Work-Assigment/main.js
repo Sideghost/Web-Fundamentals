117b4f7c1cadf1af24f9b8f586ad66619adacd03
Carlos Guilherme Pereira <74246462+Sideghost@users.noreply.github.com>
2022-11-28 11:57:12 +0100

    Refactoring from the class folder and about page with requirements

---
+++ Work-Assigment/main.js
@@ -0,0 +1,48 @@
+function selectFilter(clicked_element){
+    $(clicked_element).addClass("selected").siblings().removeClass("selected");
+    $("img").hide().filter("."+clicked_element.innerHTML).show();
+}
+
+function move() {
+    var elem = document.getElementById("bar");   
+    var width = 20;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++; 
+            elem.style.width = width + '%'; 
+            elem.innerHTML = width * 1  + '%';
+        }
+    }
+}
+
+
+/*
+function selectFilter(clicked_element){
+    console.log(clicked_element.innerHTML);
+
+    var selected_items = document.getElementsByClassName("selected");
+    for(var i = 0; i < selected_items.length; i++){
+        selected_items[i].classList.remove("selected");
+    }
+
+    clicked_element.classList.add("selected");
+
+    filterImages(clicked_element.innerHTML);
+}
+
+function filterImages(category){
+    // hide all images
+    var images = document.getElementsByTagName("img");
+    for(var i=0; i<images.length; i++){
+        images[i].style.display = "none";
+    }
+    // display images of selcted category
+    var category_images = document.getElementsByClassName(category);
+    for(var i=0; i<category_images.length; i++){
+        category_images[i].style.display = "block";
+    }
+}
+*/
