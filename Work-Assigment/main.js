5646e57a304c70f97d91f26c695ffa9bab3ed3cb
Carlos Guilherme Pereira <74246462+Sideghost@users.noreply.github.com>
2023-01-07 23:47:33 +0100

    Home and About page done

--- Work-Assigment/main.js
+++ Work-Assigment/main.js
@@ -1,3 +1,5 @@
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
 function selectFilter(clicked_element){
     $(clicked_element).addClass("selected").siblings().removeClass("selected");
     $("img").hide().filter("."+clicked_element.innerHTML).show();
@@ -18,31 +20,50 @@ function move() {
     }
 }
 
+/* image slider*/
+var counter = 1;
+setInterval(function(){
+	document.getElementById('radio' + counter).checked = true;
+	counter++;
+  	if(counter > 4){
+    	counter = 1;
+  	}
+}, 5000);
 
-/*
-function selectFilter(clicked_element){
-    console.log(clicked_element.innerHTML);
 
-    var selected_items = document.getElementsByClassName("selected");
-    for(var i = 0; i < selected_items.length; i++){
-        selected_items[i].classList.remove("selected");
+/*progress bar*/
+document.querySelector('.progress-checkbox').addEventListener('change', function() {
+    if (this.checked) {
+      document.querySelector('.progress').style.width = '100%';
+    } else {
+      document.querySelector('.progress').style.width = '0%';
     }
+  });
 
-    clicked_element.classList.add("selected");
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
-    filterImages(clicked_element.innerHTML);
+checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', () => {
+        updateProgressBar();
+    });
+});
+
+function updateProgressBar() {
+    // get all the checkboxes
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    let totalValue = 0;
+  
+    // iterate over the checkboxes and add their values if they are checked
+    checkboxes.forEach((checkbox) => {
+        if (checkbox.checked) {
+            totalValue += Number(checkbox.dataset.value)*2;
+        }
+    });
+  
+    // set the width of the progress bar based on the sum of the values
+    document.querySelector('.progress').style.width = `${totalValue}%`;
 }
 
-function filterImages(category){
-    // hide all images
-    var images = document.getElementsByTagName("img");
-    for(var i=0; i<images.length; i++){
-        images[i].style.display = "none";
-    }
-    // display images of selcted category
-    var category_images = document.getElementsByClassName(category);
-    for(var i=0; i<category_images.length; i++){
-        category_images[i].style.display = "block";
-    }
+window.onload = () => {
+    updateProgressBar();
 }
-*/
